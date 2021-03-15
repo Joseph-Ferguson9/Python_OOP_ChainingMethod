2d56d6581aa3b32ab398d7d4c08d2ebacce012d3
Joseph Ferguson <78124280+Joseph-Ferguson9@users.noreply.github.com>
2021-03-15 10:59:07 -0500

    Add files via upload

---
+++ chaining_methods.py
@@ -0,0 +1,52 @@
+class USER:
+    def __init__(self, username, email_address):
+        self.name = username
+        self.email = email_address
+        self.account_balance = 0
+    def make_deposit(self, amount):
+        self.account_balance += amount
+        return self
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+        return self
+    def display_user_balance(self):
+        print(f"{self.name}'s current account balance is ${self.account_balance}.")
+    def transfer_money(self, other_user, amount):
+        other_user.account_balance += amount
+        self.account_balance -= amount
+        return self
+
+joseph = USER("Joe Ferguson", "joegferguson@gmail.com")
+rydder = USER("Rydder Green", "rydderG@gmail.com")
+derek = USER("Derek Canoli", "thegodfather@gmail.com")
+
+# joseph.make_deposit(1400)
+# joseph.make_deposit(55)
+# joseph.make_deposit(800)
+# joseph.make_withdrawal(700)
+# joseph.display_user_balance()
+
+joseph.make_deposit(1400).make_deposit(55).make_deposit(800).make_withdrawal(700).display_user_balance()
+
+# rydder.make_deposit(1400)
+# rydder.make_deposit(500)
+# rydder.make_withdrawal(3000)
+# rydder.make_withdrawal(20)
+# rydder.display_user_balance()
+
+rydder.make_deposit(1400).make_deposit(500).make_withdrawal(3000).make_withdrawal(20).display_user_balance()
+
+# derek.make_deposit(1400)
+# derek.make_withdrawal(300)
+# derek.make_withdrawal(100)
+# derek.make_withdrawal(400)
+# derek.display_user_balance()
+
+derek.make_deposit(1400).make_withdrawal(300).make_withdrawal(100).make_withdrawal(400).display_user_balance()
+
+joseph.transfer_money(rydder, 1120)
+derek.transfer_money(rydder, 50)
+
+joseph.display_user_balance()
+rydder.display_user_balance()
+derek.display_user_balance()
